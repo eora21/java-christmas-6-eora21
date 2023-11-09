539635b6c5e1d2d8e9deb293ba2d28e97af7836d
eora21 <eora21@naver.com>
2023-11-10 01:09:44 +0900

    refactor: 💡 수정 금액은 개발자가 작성하는 부분이므로, 음수인 금액을 작성 시 처리 불가하게끔 변경

--- src/main/java/christmas/exception/NegativeAmountException.java
+++ src/main/java/christmas/exception/NegativeAmountException.java
@@ -1,6 +1,6 @@
 package christmas.exception;
 
-public class NegativeAmountException extends CustomException {
+public class NegativeAmountException extends IllegalStateException {
     public NegativeAmountException() {
         super("금액은 음수일 수 없습니다.");
     }
