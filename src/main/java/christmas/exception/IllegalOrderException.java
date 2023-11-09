badb7c4df87b383ce3bbff11c9e2c7d8d58a5dfa
eora21 <eora21@naver.com>
2023-11-09 22:19:57 +0900

    feat: 🎸 (IllegalOrderException) 추가 유효하지 않은 주문에 대한 가상 클래스 예외 작성

---
+++ src/main/java/christmas/exception/IllegalOrderException.java
@@ -0,0 +1,7 @@
+package christmas.exception;
+
+public abstract class IllegalOrderException extends CustomException {
+    public IllegalOrderException() {
+        super("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+}
