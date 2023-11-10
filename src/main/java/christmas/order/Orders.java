9b22d3d0b4c6246d929d9594ffb7e3dc3f7707fb
eora21 <eora21@naver.com>
2023-11-10 09:45:29 +0900

    feat: 🎸 (Orders) 추가 주문 메뉴 중복 검증 메서드 작성

--- src/main/java/christmas/order/Orders.java
+++ src/main/java/christmas/order/Orders.java
@@ -1,15 +1,24 @@
 package christmas.order;
 
+import christmas.exception.custom.OrderMenuDuplicateException;
 import christmas.exception.custom.OverMaxTotalOrderSizeException;
+import christmas.menu.Menu;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Orders {
     private static final int MAX_TOTAL_ORDER_SIZE = 20;
     private final List<OrderDetail> orderDetails;
 
     public Orders(List<OrderDetail> orderDetails) {
-        validateTotalOrderSize(orderDetails);
+        validate(orderDetails);
         this.orderDetails = orderDetails;
+    }
+
+    private void validate(List<OrderDetail> orderDetails) {
+        validateTotalOrderSize(orderDetails);
+        validateOrderMenuDuplicate(orderDetails);
     }
 
     private void validateTotalOrderSize(List<OrderDetail> orderDetails) {
@@ -20,6 +29,16 @@ public class Orders {
 
         if (MAX_TOTAL_ORDER_SIZE < totalOrderSize) {
             throw new OverMaxTotalOrderSizeException(MAX_TOTAL_ORDER_SIZE);
+        }
+    }
+
+    private void validateOrderMenuDuplicate(List<OrderDetail> orderDetails) {
+        Set<Menu> orderMenuGroup = orderDetails.stream()
+                .map(OrderDetail::getMenu)
+                .collect(Collectors.toUnmodifiableSet());
+
+        if (orderDetails.size() != orderMenuGroup.size()) {
+            throw new OrderMenuDuplicateException();
         }
     }
 }
