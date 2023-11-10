c225b58ff39acfccfca971738a6ebd709b88b498
eora21 <eora21@naver.com>
2023-11-10 12:53:37 +0900

    test: 💍 추가 음료만 주문했을 시 예외 발생 테스트 작성

--- src/test/java/christmas/order/OrdersTest.java
+++ src/test/java/christmas/order/OrdersTest.java
@@ -3,6 +3,7 @@ package christmas.order;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import christmas.exception.non_fatal.OrderOnlyOneMenuGroupException;
 import christmas.exception.non_fatal.OverMaxTotalOrderQuantityException;
 import christmas.exception.non_fatal.illegal_order.OrderMenuDuplicateException;
 import christmas.menu.Menu;
@@ -26,6 +27,16 @@ class OrdersTest {
         assertThrows(OrderMenuDuplicateException.class, () -> new Orders(List.of(
                 new OrderDetail(Menu.CHOCOLATE_CAKE, 15),
                 new OrderDetail(Menu.CHOCOLATE_CAKE, 1))
+        ));
+    }
+
+    @Test
+    @DisplayName("음료만 주문하면 예외가 발생한다.")
+    void createOrdersOnlyDrink() {
+        assertThrows(OrderOnlyOneMenuGroupException.class, () -> new Orders(List.of(
+                new OrderDetail(Menu.ZERO_COLA, 15),
+                new OrderDetail(Menu.RED_WINE, 2),
+                new OrderDetail(Menu.CHAMPAGNE, 1))
         ));
     }
 
