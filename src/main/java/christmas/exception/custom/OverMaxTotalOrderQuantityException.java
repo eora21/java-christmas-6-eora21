2ea0b542a334c966319444aedb616ae1f24f17a1
eora21 <eora21@naver.com>
2023-11-10 12:02:04 +0900

    refactor: 💡 수정 메뉴 최대 주문 수는 고객에게 안내되어야 하기에 커스텀 예외를 상속하도록 변경

--- src/main/java/christmas/exception/custom/OverMaxTotalOrderQuantityException.java
+++ src/main/java/christmas/exception/custom/OverMaxTotalOrderQuantityException.java
@@ -1,7 +1,7 @@
 package christmas.exception.custom;
 
-public class OverMaxTotalOrderQuantityException extends IllegalOrderException {
+public class OverMaxTotalOrderQuantityException extends CustomException {
     public OverMaxTotalOrderQuantityException(int totalOrderSize) {
-
+        super("메뉴는 한 번에 최대 " + totalOrderSize + "개까지만 주문할 수 있습니다.");
     }
 }
