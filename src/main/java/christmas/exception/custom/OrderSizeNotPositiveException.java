5f3660ffa14bed29ba170506962d3ff1182aef15
eora21 <eora21@naver.com>
2023-11-10 09:40:33 +0900

    refactor: 💡 수정 요구사항에 맞게 주문 개수와 최대 주문 개수를 넘었을 시 같은 메시지 표기하도록 변경

--- src/main/java/christmas/exception/custom/OrderSizeNotPositiveException.java
+++ src/main/java/christmas/exception/custom/OrderSizeNotPositiveException.java
@@ -1,7 +1,5 @@
 package christmas.exception.custom;
 
-public class OrderSizeNotPositiveException extends CustomException {
-    public OrderSizeNotPositiveException() {
-        super("주문 개수는 양수여야 합니다.");
-    }
+public class OrderSizeNotPositiveException extends IllegalOrderException {
+
 }
