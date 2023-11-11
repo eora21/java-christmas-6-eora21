b637c3d41e11aa0a78cc0006854f612bde2b035e
eora21 <eora21@naver.com>
2023-11-11 23:25:26 +0900

    feat: 🎸 (ChristmasGiveawayPromotion) 추가 크리스마스 증정 이벤트 작성

---
+++ src/main/java/christmas/promotion/giveaway_promotion/ChristmasGiveawayPromotion.java
@@ -0,0 +1,38 @@
+package christmas.promotion.giveaway_promotion;
+
+import christmas.information.Amount;
+import christmas.information.Quantity;
+import christmas.menu.Menu;
+import christmas.order.Orders;
+import christmas.promotion.information.Giveaway;
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class ChristmasGiveawayPromotion implements GiveawayPromotion {
+    private ChristmasGiveawayPromotion() {
+    }
+
+    private static class PromotionInitializer {
+        private static final ChristmasGiveawayPromotion INSTANCE = new ChristmasGiveawayPromotion();
+    }
+
+    public ChristmasGiveawayPromotion getInstance() {
+        return PromotionInitializer.INSTANCE;
+    }
+
+    private static final Giveaway GIVEAWAY_PROMOTION = new Giveaway(Menu.CHAMPAGNE, new Quantity(1));
+    private static final Amount GIVEAWAY_MIN_AMOUNT = new Amount(120_000);
+
+    @Override
+    public Optional<Giveaway> getGiveaway(LocalDate localDate, Orders orders) {
+        if (moreThanOrEqualToGiveawayMinAmount(orders.getOrdersTotalAmount())) {
+            return Optional.of(GIVEAWAY_PROMOTION);
+        }
+
+        return Optional.empty();
+    }
+
+    private boolean moreThanOrEqualToGiveawayMinAmount(Amount ordersTotalAmount) {
+        return GIVEAWAY_MIN_AMOUNT.compareTo(ordersTotalAmount) <= 0;
+    }
+}
