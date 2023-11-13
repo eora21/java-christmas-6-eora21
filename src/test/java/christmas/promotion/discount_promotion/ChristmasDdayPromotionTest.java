ea48442a620080191227123fb820a0f4e8aeb226
eora21 <eora21@naver.com>
2023-11-13 09:43:55 +0900

    test: 💍 수정 크리스마스 디데이 이벤트 싱글톤이 아닌 객체 기준으로 검증하도록 변경

--- src/test/java/christmas/promotion/discount_promotion/ChristmasDdayPromotionTest.java
+++ src/test/java/christmas/promotion/discount_promotion/ChristmasDdayPromotionTest.java
@@ -6,6 +6,7 @@ import christmas.information.Amount;
 import christmas.menu.Menu;
 import christmas.order.OrderDetail;
 import christmas.order.Orders;
+import christmas.promotion.Promotion;
 import christmas.promotion.information.Discount;
 import java.time.LocalDate;
 import java.util.List;
@@ -19,21 +20,21 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class ChristmasDdayPromotionTest {
-    ChristmasDdayPromotion christmasDdayEvent = ChristmasDdayPromotion.getInstance();
+    Promotion christmasDdayPromotion = new ChristmasDdayPromotion();
     Orders orders = new Orders(List.of(new OrderDetail(Menu.ICE_CREAM, 1)));
 
     @ParameterizedTest
     @DisplayName("12월 1일 이전, 12월 25일 이후에 이벤트가 동작될 시 빈 옵셔널을 반환한다.")
     @CsvSource(value = {"2023-11-30", "2023-12-26"})
     void calculateDiscountAmountNotInEventDuration(LocalDate localDate) {
-        assertThat(christmasDdayEvent.calculateDiscount(localDate, orders))
+        assertThat(christmasDdayPromotion.calculateDiscount(localDate, orders))
                 .isEmpty();
     }
 
     @Test
     @DisplayName("크리스마스 디데이 이벤트는 증정 이벤트를 하지 않는다.")
     void getGiveawayEmpty() {
-        assertThat(christmasDdayEvent.getGiveaway(LocalDate.parse("2023-12-02"), orders))
+        assertThat(christmasDdayPromotion.getGiveaway(LocalDate.parse("2023-12-02"), orders))
                 .isEmpty();
     }
 
@@ -41,8 +42,9 @@ class ChristmasDdayPromotionTest {
     @DisplayName("12월 1일의 할인 금액은 1,000원이며, 하루가 지날 때마다 할인 금액이 100원씩 올라간다.")
     @MethodSource("localDateAndExpectDiscountAmountValue")
     void calculateDiscountAmount(LocalDate localDate, int expectDiscountAmountValue) {
-        Optional<Discount> discountOptional = christmasDdayEvent.calculateDiscount(localDate, orders);
-        assertThat(discountOptional).hasValue(new Discount(new Amount(expectDiscountAmountValue)));
+        Optional<Discount> discountOptional = christmasDdayPromotion.calculateDiscount(localDate, orders);
+        Discount discount = discountOptional.orElseThrow();
+        assertThat(discount.getDiscountAmount()).isEqualTo(new Amount(expectDiscountAmountValue));
     }
 
     static Stream<Arguments> localDateAndExpectDiscountAmountValue() {
