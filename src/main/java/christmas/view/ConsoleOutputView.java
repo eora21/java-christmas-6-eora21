4e5d535cdcd636650155b69cc9fbc514e188c96e
eora21 <eora21@naver.com>
2023-11-14 22:44:30 +0900

    feat: 🎸 (ConsoleOutputView) 추가 우테코 식당 이벤트 플래너 안내메시지 출력 메서드 작성

---
+++ src/main/java/christmas/view/ConsoleOutputView.java
@@ -0,0 +1,68 @@
+package christmas.view;
+
+import christmas.information.Amount;
+import christmas.order.Orders;
+import christmas.promotion.PromotionBadge;
+import christmas.promotion.information.Benefit;
+import christmas.promotion.information.Giveaway;
+import christmas.promotion.information.PromotionBenefitInfo;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class ConsoleOutputView implements OutputView {
+    @Override
+    public void printGreeting(Month month) {
+        System.out.println("안녕하세요! 우테코 식당 " + month.getValue() + " 월 이벤트 플래너입니다.");
+    }
+
+    @Override
+    public void requestVisitDay(Month month, String visitDayForm) {
+
+    }
+
+    @Override
+    public void printPreviewBenefitsMessage(LocalDate localDate) {
+
+    }
+
+    @Override
+    public void requestOrderRequests(String orderRequestsExample) {
+
+    }
+
+    @Override
+    public void printOrders(Orders orders) {
+
+    }
+
+    @Override
+    public void printBeforeDiscountAmount(Amount beforeDiscountAmount) {
+
+    }
+
+    @Override
+    public void printGiveaways(List<Giveaway> giveaways) {
+
+    }
+
+    @Override
+    public void printPromotionBenefitInfo(PromotionBenefitInfo promotionBenefitInfo) {
+
+    }
+
+    @Override
+    public void printTotalBenefit(Benefit totalBenefit) {
+
+    }
+
+    @Override
+    public void printAfterDiscountAmount(Amount afterDiscountAmount) {
+
+    }
+
+    @Override
+    public void printBadge(PromotionBadge badge) {
+
+    }
+}
