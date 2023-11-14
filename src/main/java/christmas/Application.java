1695bab85388ec168d8e8ad384a62613b6babf69
eora21 <eora21@naver.com>
2023-11-15 00:16:14 +0900

    feat: 🎸 (Application) 추가 View, 플래너 연결 후 동작시키는 메서드 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+import christmas.planner.PromotionPlaner;
+import christmas.view.ConsoleInputView;
+import christmas.view.ConsoleOutputView;
+import christmas.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = new View(new ConsoleInputView(), new ConsoleOutputView());
+        PromotionPlaner promotionPlaner = new PromotionPlaner(view, 2023, 12);
+        promotionPlaner.runPlaner();
     }
 }
