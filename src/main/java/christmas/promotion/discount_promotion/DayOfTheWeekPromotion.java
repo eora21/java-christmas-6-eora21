e88b9634d51ec869111d7a2847ae0e31a58479ed
eora21 <eora21@naver.com>
2023-11-14 16:34:49 +0900

    refactor: 💡 (DayOfTheWeekPromotion) 수정 프로모션 확장, 메뉴별 할인값 반환으로 변경

--- src/main/java/christmas/promotion/discount_promotion/DayOfTheWeekPromotion.java
+++ src/main/java/christmas/promotion/discount_promotion/DayOfTheWeekPromotion.java
@@ -1,15 +1,21 @@
 package christmas.promotion.discount_promotion;
 
 import christmas.information.Amount;
+import christmas.information.Quantity;
+import christmas.menu.Menu;
 import christmas.menu.MenuGroup;
+import christmas.order.OrderDetail;
 import christmas.order.Orders;
+import christmas.promotion.Promotion;
 import christmas.promotion.information.Discount;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class DayOfTheWeekPromotion implements DiscountPromotion {
+public class DayOfTheWeekPromotion implements Promotion {
     private static final Amount UNIT_DISCOUNT_AMOUNT = new Amount(2_023);
     private final List<DayOfWeek> dayOfWeeks;
     private final MenuGroup menuGroup;
@@ -20,28 +26,24 @@ public class DayOfTheWeekPromotion implements DiscountPromotion {
     }
 
     @Override
-    public Optional<Discount> calculateDiscount(LocalDate localDate, Orders orders) {
+    public Map<Menu, Discount> calculateMenuDiscount(LocalDate localDate, Orders orders) {
         DayOfWeek dayOfWeek = localDate.getDayOfWeek();
 
         if (dayOfWeeks.contains(dayOfWeek)) {
-            return calculateDiscount(orders);
+            return calculateDiscountInMenuGroup(orders);
         }
 
-        return Optional.empty();
+        return Collections.emptyMap();
     }
 
-    private Optional<Discount> calculateDiscount(Orders orders) {
-        int totalMenuGroupQuantity = orders.getTotalMenuGroupQuantity(menuGroup);
-        Amount discountAmount = UNIT_DISCOUNT_AMOUNT.multiplyAmount(totalMenuGroupQuantity);
-        return existDiscountAmount(discountAmount);
+    private Map<Menu, Discount> calculateDiscountInMenuGroup(Orders orders) {
+        return orders.filterOrderDetailUsingMenuGroup(menuGroup).stream()
+                .collect(Collectors.toUnmodifiableMap(
+                        OrderDetail::getMenu,
+                        orderDetail -> discountMultiplyQuantity(orderDetail.getQuantity())));
     }
 
-    private Optional<Discount> existDiscountAmount(Amount discountAmount) {
-        Amount zeroAmount = Amount.createZeroAmount();
-        if (zeroAmount.equals(discountAmount)) {
-            return Optional.empty();
-        }
-
-        return Optional.of(new Discount(discountAmount));
+    private Discount discountMultiplyQuantity(Quantity quantity) {
+        return new Discount(UNIT_DISCOUNT_AMOUNT.multiplyAmount(quantity.quantity()));
     }
 }
