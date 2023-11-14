f718cb8ef42ec10e79ec053a57c933630ef00c70
eora21 <eora21@naver.com>
2023-11-14 22:12:15 +0900

    feat: 🎸 (InputView) 추가 출력에게 넘겨줄 설명에 대한 메서드 작성

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,7 +4,11 @@ import christmas.order.OrderRequest;
 import java.util.List;
 
 public interface InputView {
+    String requireVisitDayForm();
+
     int enterVisitDay();
+
+    String requireOrderRequestsExample();
 
     List<OrderRequest> enterOrderRequests();
 }
