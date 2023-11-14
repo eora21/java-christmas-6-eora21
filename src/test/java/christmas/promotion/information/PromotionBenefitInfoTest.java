36a50984747873954f076209765d07b801dddd20
eora21 <eora21@naver.com>
2023-11-14 17:03:43 +0900

    test: 💍 추가 모든 프로모션 혜택의 총 합 계산 테스트 작성

---
+++ src/test/java/christmas/promotion/information/PromotionBenefitInfoTest.java
@@ -0,0 +1,24 @@
+package christmas.promotion.information;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.information.Amount;
+import christmas.promotion.PromotionPlan;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PromotionBenefitInfoTest {
+    @Test
+    @DisplayName("모든 프로모션 혜택의 총 합을 계산한다.")
+    void calculateTotalBenefit() {
+        PromotionBenefitInfo promotionBenefitInfo = new PromotionBenefitInfo(Map.of(
+                PromotionPlan.WEEKDAY_PROMOTION.getPromotion(), new Benefit(new Amount(1_000)),
+                PromotionPlan.WEEKEND_PROMOTION.getPromotion(), new Benefit(new Amount(2_000)),
+                PromotionPlan.CHRISTMAS_GIVEAWAY_PROMOTION.getPromotion(), new Benefit(new Amount(3_000))
+        ));
+
+        Benefit totalBenefit = promotionBenefitInfo.getTotalBenefit();
+        assertThat(totalBenefit.getBenefitAmount()).isEqualTo(new Amount(6_000));
+    }
+}
