152caa7c2a65dd6734543a3a7a86666c6cd6ad74
eora21 <eora21@naver.com>
2023-11-14 22:46:57 +0900

    feat: 🎸 (View) 추가 안내 메시지를 보여주는 메서드 작성

---
+++ src/main/java/christmas/view/View.java
@@ -0,0 +1,17 @@
+package christmas.view;
+
+import java.time.Month;
+
+public class View {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public View(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void showGreeting(Month month) {
+        outputView.printGreeting(month);
+    }
+}
