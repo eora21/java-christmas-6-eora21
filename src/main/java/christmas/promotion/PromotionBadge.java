59947e303cc2d405367ebc409a3fd6e01d916150
eora21 <eora21@naver.com>
2023-11-15 10:57:35 +0900

    refactor: 💡 (PromotionBadge) 수정 Optional이 아닌 배지 자체를 반환하도록 변경

--- src/main/java/christmas/promotion/PromotionBadge.java
+++ src/main/java/christmas/promotion/PromotionBadge.java
@@ -6,6 +6,8 @@ import java.util.Comparator;
 import java.util.Optional;
 
 public enum PromotionBadge {
+
+    NONE("없음", new Benefit(0)),
     STAR("별", new Benefit(5_000)),
     TREE("트리", new Benefit(10_000)),
     SANTA("산타", new Benefit(20_000));
@@ -18,12 +20,13 @@ public enum PromotionBadge {
         this.benefit = benefit;
     }
 
-    public static Optional<PromotionBadge> getBadge(Benefit benefit) {
+    public static PromotionBadge getBadge(Benefit benefit) {
         return Arrays.stream(PromotionBadge.values())
                 .sorted(Comparator.comparing(promotionBadge -> promotionBadge.benefit))
                 .sorted(Comparator.reverseOrder())
                 .filter(promotionBadge -> isGreaterOrEqual(benefit, promotionBadge))
-                .findFirst();
+                .findFirst()
+                .orElse(NONE);
     }
 
     private static boolean isGreaterOrEqual(Benefit benefit, PromotionBadge promotionBadge) {
