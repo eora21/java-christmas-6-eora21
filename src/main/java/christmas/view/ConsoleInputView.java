4ee78c38fb8caa941438e43585dc8c0b2ae52dad
eora21 <eora21@naver.com>
2023-11-15 12:47:07 +0900

    refactor: 💡 (ConsoleInputView) 수정 사용자 입력값 변환 중 잘못된 값에 의한 예외 작성

--- src/main/java/christmas/view/ConsoleInputView.java
+++ src/main/java/christmas/view/ConsoleInputView.java
@@ -2,6 +2,8 @@ package christmas.view;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import christmas.exception.recoverable.MismatchOrderRequestException;
+import christmas.exception.recoverable.ParseIntException;
 import christmas.order.OrderRequest;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ public class ConsoleInputView implements InputView {
     private static final String VISIT_DAY_FORM = "숫자만 입력해 주세요!";
     private static final String ORDER_REQUEST_DELIMITER = ",";
     private static final String NAME_QUANTITY_DELIMITER = "-";
+    private static final int NAME_QUANTITY_TOKEN_SIZE = 2;
 
     @Override
     public String requireVisitDayForm() {
@@ -20,7 +23,15 @@ public class ConsoleInputView implements InputView {
 
     @Override
     public int enterVisitDay() {
-        return Integer.parseInt(readLine());
+        return parseInt(readLine());
+    }
+
+    private int parseInt(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException ignore) {
+            throw new ParseIntException();
+        }
     }
 
     @Override
@@ -53,8 +64,13 @@ public class ConsoleInputView implements InputView {
 
     private OrderRequest convertTokenToOrderRequest(String token) {
         StringTokenizer orderRequestTokenizer = new StringTokenizer(token, NAME_QUANTITY_DELIMITER);
+
+        if (orderRequestTokenizer.countTokens() != NAME_QUANTITY_TOKEN_SIZE) {
+            throw new MismatchOrderRequestException();
+        }
+
         String orderMenuName = orderRequestTokenizer.nextToken().trim();
-        int orderQuantity = Integer.parseInt(orderRequestTokenizer.nextToken().trim());
+        int orderQuantity = parseInt(orderRequestTokenizer.nextToken().trim());
         return new OrderRequest(orderMenuName, orderQuantity);
     }
 }
